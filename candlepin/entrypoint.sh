db486cbe17abcf52a0304a17554aa8254d472209
Jason Galens <jason@galensfamily.org>
2025-05-07 04:04:21 +0000

    Work on candlepin build

--- candlepin/entrypoint.sh
+++ candlepin/entrypoint.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-TRUSTSTORE=/etc/candlepin/certs/truststore
-KEYSTORE=/etc/candlepin/certs/keystore
+set -e
 
-PASSWORD_DIR=/var/run/secrets/passwords
-KEYSTORE_PASSWORD=$PASSWORD_DIR/KEYSTORE_PASSWORD
-TRUSTSTORE_PASSWORD=$PASSWORD_DIR/TRUSTSTORE_PASSWORD
+TRUSTSTORE=${TRUSTSTORE:-/etc/candlepin/certs/truststore}
+KEYSTORE=${KEYSTORE:-/etc/candlepin/certs/keystore}
 
 function add_key {
   local pkcs12=/tmp/pkcs12
@@ -16,17 +14,14 @@ function add_key {
 
   openssl pkcs12 -export \
     -in $cert -inkey $key -out $pkcs12 \
-    -name $alias -CAfile $ca -password file:$KEYSTORE_PASSWORD
+    -name $alias -CAfile $ca -password pass:$KEYSTORE_PASSWORD
 
   keytool -importkeystore -noprompt \
-    -srckeystore $pkcs12 -srcstorepass:file $KEYSTORE_PASSWORD \
-    -destkeystore $KEYSTORE -destkeystorepass:file $KEYSTORE_PASSWORD \
+    -srckeystore $pkcs12 -srcstorepass $KEYSTORE_PASSWORD \
+    -destkeystore $KEYSTORE -deststorepass $KEYSTORE_PASSWORD \
     -srcalias $alias -destalias $alias -J-Dcom.redhat.fips=false
 
   rm -f $pkcs12
-
-  chown root:tomcat $KEYSTORE
-  chmod 0640 $KEYSTORE
 }
 
 function add_trust {
@@ -34,11 +29,8 @@ function add_trust {
   local certificate=$2
 
   keytool -import -noprompt -storetype pkcs12 -keystore $TRUSTSTORE \
-    -alias $alias -file $certificate -storepass:file $TRUSTSTORE_PASSWORD \
+    -alias $alias -file $certificate -storepass $TRUSTSTORE_PASSWORD \
     -J-Dcom.redhat.fips=false
-
-  chown root:tomcat $TRUSTSTORE
-  chmod 0640 $TRUSTSTORE
 }
 
 add_trust candlepin-ca /var/run/secrets/tomcat/ca.crt
@@ -48,5 +40,12 @@ add_key tomcat /var/run/secrets/tomcat/tls.crt /var/run/secrets/tomcat/tls.key /
 
 cp /var/run/secrets/ca/tls.crt /etc/candlepin/certs/candlepin-ca.crt
 cp /var/run/secrets/ca/tls.key /etc/candlepin/certs/candlepin-ca.key
+
+openssl x509 -noout -subject -nameopt rfc2253,sep_comma_plus_space < /var/run/secrets/foreman/tls.crt | sed 's/^subject/katelloUser/' > /opt/tomcat/conf/cert-users.properties
+
+export POSTGRES_URL="jdbc:postgresql://${POSTGRES_HOST}:${POSTGRES_PORT:-5432}/${POSTGRES_DATABASE:-candlepin}"
+export POSTGRES_USER=${POSTGRES_USER:-candlepin}
+
+envsubst < /etc/candlepin/candlepin.conf.template > /etc/candlepin/candlepin.conf
 
 exec $@
